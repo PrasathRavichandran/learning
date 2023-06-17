f757bcf160e392ede1ac26554c602d6aea97ef06
prasathfibonalabs <133684042+prasathfibonalabs@users.noreply.github.com>
2023-06-17 12:32:53 +0530

    studied const

--- c++/helloworld.cpp
+++ c++/helloworld.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
-int main(){
+int main()
+{
+    // Datatypes
+    int age = 25;
 
-    std::cout << "I like coding!" << std::endl;
-    std::cout << "I like pizza more then other food!";
+    double salary = 9.5;
+
+    char grade = 'A';
+
+    // string (object that represents a sequence of text)
+    // std::string name = "Prasath";
+    // std::string address = "kovilpatti, Tamilnadu";
+    // std::cout << "My name is " << name << " and my age is " << age << ". I am from " << address << "and my yearly CTA is Rs: " << salary << "L.";
+
+    // const - read only value
+    const double PI = 3.14;
+    double radius = 34;
+    double circumference = 2 * PI * radius;
+
+    std::cout << circumference << "cm";
 
     return 0;
 }
